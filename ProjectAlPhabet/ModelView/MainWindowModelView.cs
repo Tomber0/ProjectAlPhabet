590df872ff6f45da51968da5349fb5899bc3a87a
llzai <llzaicev@yandex.ru>
2024-02-10 03:11:21 +0300

    added buttons

--- ProjectAlPhabet/ModelView/MainWindowModelView.cs
+++ ProjectAlPhabet/ModelView/MainWindowModelView.cs
@@ -1,9 +1,31 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ProjectAlPhabet.ModelView
 {
     public partial class MainWindowModelView:ObservableObject
     {
+        [ObservableProperty]
+        private string _settingsTitle = "Show Settings";
+
+        [RelayCommand]
+        private async Task NextPictureAsync() 
+        {
+            throw new NotImplementedException();
+        }
+
+        [RelayCommand]
+        private async Task PostPictureAsync() 
+        {
+            throw new NotImplementedException();
+        }
+
+        [RelayCommand]
+        private async Task ShowSettingsAsync() 
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 }
